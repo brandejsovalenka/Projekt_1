ddf089da47c44d042f1840f3882fa605b850a36d
brandejsovalenka <brandejsova7@seznam.cz>
2024-04-24 17:17:47 +0200

    pridana funkce print na radku 14

--- skript1.py
+++ skript1.py
@@ -1,6 +1,14 @@
-print("ahoj studenti Python Akademie")
+print("Zaciname gitovat")
+
+list1 = (1, 2, 3, 4, 5)
+
+dictionary = ("one" : 1)
+
+print("Hello World")
 
 print("Provedli jsme zmeny")
 
-print("Dalsi zmeny")
+print("Pridal jsem dalsi print")
 
+
+print("Projekt 1")
